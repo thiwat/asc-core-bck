4c04cc987896b4f33a299a23daf48db84da8d1a7
Thiwat Nuntjaruwong <thiwat@shopstack.asia>
2024-07-15 23:06:06 +0700

    Init Project

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+
+	"asc-core/configs"
+	"asc-core/event"
+	"asc-core/user"
+
+	fiber "github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	api := app.Group("/api")
+	v1 := api.Group("/v1")
+
+	event.RestRouteV1(v1)
+	user.RestRouteV1(v1)
+
+	log.Fatal(app.Listen(configs.GetPort()))
+}
